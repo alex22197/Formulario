7def5eb48ff3c8bd423a7e109cf69b4fcdfc18ff
alex22197 <alexbalonmano97@gmail.com>
2018-02-03 22:12:36 +0100

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,20 @@
+	
+	function myFunction() {
+    	location.href="formulario/index.html";
+	}
+
+function obtenerCookie(email) {
+    var name = email + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+if(obtenerCookie('sesion')!=''){
+
+	
+}
